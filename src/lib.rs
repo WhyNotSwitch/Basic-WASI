3b92df0f0a8709e20fa78a56729658ea58ecc126
Godwin Igwurube <godwinaquinas@gmail.com>
2023-01-23 17:15:02 +0100

    fix commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 
+use sdk::{ get_data, log_info};
+
 mod sdk;
 
 #[no_mangle]
@@ -12,24 +14,24 @@ pub extern "C" fn alloc(size: i32) -> *mut u8 {
 #[no_mangle]
 pub extern "C" fn start(_resource_id: i32) -> i32 {
     let str = String::from("hello world");
-    sdk::log_info(&str);
+    log_info(&str);
     return 0;
 }
 
 #[no_mangle]
 pub extern "C" fn handle_event(_resource_id: i32) -> i32 {
-    sdk::log_info(
+    log_info(
         &format!("Handler called with event_id: {}", _resource_id));
 
     let payload = get_data_as_str(_resource_id);
-    sdk::log_info(&format!("event data as string: {}", payload));
+    log_info(&format!("event data as string: {}", payload));
     return 0;
 }
 
 // Returns the event payload as a string
 fn get_data_as_str(event_id: i32) -> String {
 
-    return match sdk::get_data(event_id) {
+    return match get_data(event_id) {
         Some(data) => match String::from_utf8(data) {
             Ok(data) => data,
             _ => String::from("convert from utf8 to string failed")
