aed609669999bcfb8d5b576ce5327164728f7336
Godwin Igwurube <godwinaquinas@gmail.com>
2023-01-23 16:38:33 +0100

    error modification

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@
 mod sdk;
 
 #[no_mangle]
+pub extern "C" fn alloc(size: i32) -> *mut u8 {
+    let mut buf: Vec<u8> = Vec::with_capacity(size as _);
+    let ptr = buf.as_mut_ptr();
+    std::mem::forget(buf);
+    return ptr;
+}
+
+#[no_mangle]
 pub extern "C" fn start(_resource_id: i32) -> i32 {
     let str = String::from("hello world");
     sdk::log_info(&str);
