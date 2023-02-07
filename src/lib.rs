ae699afe8234d671a179890ae24d1c58542ef8db
Godwin Igwurube <godwinaquinas@gmail.com>
2023-02-07 15:58:12 +0100

    contract call check

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,53 @@
 
-// use core::slice::SlicePattern;
-
-// use std::collections::HashMap
-
 use anyhow::{bail, Result};
+use ethabi::{Contract, Token, ethereum_types::U256};
 use serde_json::Value;
 use serde::{Serialize, Deserialize};
+use lazy_static::lazy_static;
 
-// use reqwest_wasm::Client;
-use bytes::Bytes;
-use http;
-use wasi_experimental_http;
+#[allow(dead_code)]
+const CONTRACT_ADDRESS: &str = "0xb963C2c5c9d023F61E8338B85fFd58Bd0663D545";
+
+const ABI_STR: &str = r#"
+    [
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "num",
+                    "type": "uint256"
+                }
+            ],
+            "name": "hello",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "pure",
+            "type": "function"
+        }
+    ]
+"#;
+
+lazy_static! {
+    static ref SIMPLE_CONTRACT: Contract = serde_json::from_str(ABI_STR).unwrap();
+}
+
 
 extern "C" {
     fn ws_log(log_level: i32, ptr: *const u8, size: i32) -> i32;
     fn ws_get_data(event_id: i32, return_ptr: *const *mut u8, return_size: *const i32) -> i32;
     fn ws_set_db(key_ptr: *const u8, key_size: i32, value_ptr: *const u8, value_size: i32) -> i32;
     fn ws_get_db(hey_ptr: *const u8, key_size: i32, return_ptr: *const *mut u8, return_size: &i32) -> i32;
+    fn ws_call_contract(
+        ptr: *const u8,
+        size: i32,
+        return_ptr: *const *mut u8,
+        return_size: *const i32,
+    ) -> i32;
 }
 
 #[no_mangle]
@@ -95,24 +126,58 @@ pub extern "C" fn handle_get_db_event(event_id: i32) -> i32 {
 pub extern "C" fn handle_confirmation_event(event_id: i32) -> i32 {
     log_info(&format!("Handler called with event_id: {}", event_id));
 
-    // let payload = match get_data(event_id) {
-    //     Some(data) => match serde_json::from_slice::<ConfirmationPayload>(data.as_slice()) {
-    //         Ok(data)=> data,
-    //         Err(error) => {
-    //             log_info(&format!("failed to read data json with error {}", error.to_string()));
-    //             return -1;
-    //         }
-    //     },
-    //     None => {
-    //         log_info(&"failed to get data");
-    //         return -1;
-    //     }
-    // };
+    let to = CONTRACT_ADDRESS.to_string();
 
-    send_confirmation_check();
-    return 0;
+    let data = match encode_call_fn(32u64.into()) {
+        Ok(res) => res,
+        Err(error) => panic!("could not encode function call data, failed with error: {}", error)
+    };
+
+    match call_contract(&to, &hex::encode(data)) {
+        None => log_info("nothing gotten from call"),
+        Some(ret) => decode_call_fn(ret).expect("ouput decode failed")
+    }
+
+    0
 }
 
+fn encode_call_fn(param: U256) -> Result<Vec<u8>, ethabi::Error>{
+    return SIMPLE_CONTRACT
+        .function("hello")?
+        .encode_input(&[Token::Uint(param)]);
+}
+
+fn decode_call_fn(ret: Vec<u8>) -> Result<()> {
+
+    let tokens = SIMPLE_CONTRACT
+        .function("hello")?
+        .decode_output(&ret)?;
+
+    log_info(format!("got out put {:?}", tokens).as_str());
+    Ok(())
+}
+
+#[derive(Serialize, Debug)]
+struct Call {
+    to: String,
+    data: String
+}
+
+
+pub fn call_contract(to: &String, data: &String) -> Option<Vec<u8>> {
+    let data_ptr = &mut (0 as i32) as *const _ as *const *mut u8;
+    let data_size = &(0 as i32);
+
+    let tx = Call {
+        to: to.clone(),
+        data: data.clone(),
+    };
+    let str = serde_json::to_string(&tx).ok()?;
+    match unsafe { ws_call_contract(str.as_ptr(), str.len() as _, data_ptr, data_size) } {
+        0 => Some(unsafe { Vec::from_raw_parts(*data_ptr, *data_size as _, *data_size as _) }),
+        _ => None,
+    }
+}
 
 pub fn log_info(str: &str) {
     unsafe { ws_log(3, str.as_bytes().as_ptr(), str.len() as _) };
@@ -166,34 +231,6 @@ fn sink_data(data: &Value) -> Result<()> {
     });
 
     set_db(&id, serde_json::to_string(&value)?.into_bytes())
-}
-
-fn send_confirmation_check() {
-
-    let url = "https://postman-echo.com/post".to_string();
-    log_info(&"no fail 1");
-    let req = http::request::Builder::new()
-        .method(http::Method::POST)
-        .uri(&url)
-        .header("Content-Type", "text/plain")
-        .header("abc", "def");
-
-    log_info(&"no fail 2");
-    let b = Bytes::from("Testing with a request body. Does this actually work?");
-    log_info(&"no fail 3");
-    let req = req.body(Some(b)).unwrap();
-    log_info(&"no fail 4");
-    let res = wasi_experimental_http::request(req).expect("cannot make request");
-
-    // let res_body = match res.body_read_all() {
-    //     Ok(data) => data,
-    //     Err(error) => panic!()
-    // };
-
-    // let str = std::str::from_utf8(&res.body_read_all()).unwrap().to_string();
-    // log_info(&format!("{:#?}", res.header_get("Content-Type".to_string())));
-    // log_info(&format!("{}", str));
-    log_info(&format!("{:#?}", res.status_code));
 }
 
 // Expected Value format in json
